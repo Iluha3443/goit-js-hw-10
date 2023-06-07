7db7d74540fed977812bcd7bce37ac1c45b90bde
Iluha3443 <tkacenkoila01@gmail.com>
2023-06-07 21:34:55 +0300

    task2

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,7 +5,13 @@ export function fetchBreeds() {
 };
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    const API_KEY = 'live_VY16isoMfVwTscFXp0PO3sLHtazvEwsNGlEIbRiYRdilDHkpPGgsoL2LiHXQRVTb';
+    const headers = {
+        'x-api-key': API_KEY
+    };
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+    
+    return fetch(url, { headers })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error');
